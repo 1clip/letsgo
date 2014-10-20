54a3df4392704fd05189924a573a4689d38b6477
xbwu <xbwu.dev@gmail.com>
2014-10-19 23:21:00 -0700

    hangout store interface

--- hangout/hangout-store/src/main/java/coffee/letsgo/hangout/store/HangoutStore.java
+++ hangout/hangout-store/src/main/java/coffee/letsgo/hangout/store/HangoutStore.java
@@ -1,7 +1,23 @@
 package coffee.letsgo.hangout.store;
 
+import coffee.letsgo.hangout.store.model.HangoutData;
+import coffee.letsgo.hangout.store.model.HangoutFolkData;
+
+import java.util.List;
+
 /**
  * Created by xbwu on 10/19/14.
  */
 public interface HangoutStore {
+    void setHangout(HangoutData hangoutData);
+
+    void setHangoutFolk(HangoutFolkData hangoutFolkData);
+
+    void setHangoutFolks(List<HangoutFolkData> hangoutFolks);
+
+    HangoutData getHangout(long hangoutId);
+
+    List<HangoutFolkData> getHangoutFolks(long hangoutId);
+
+    HangoutFolkData getHangoutFolk(long hangoutId, long userId);
 }
--- hangout/hangout-store/src/main/java/coffee/letsgo/hangout/store/HangoutStoreCassandraImpl.java
+++ hangout/hangout-store/src/main/java/coffee/letsgo/hangout/store/HangoutStoreCassandraImpl.java
@@ -1,7 +1,52 @@
 package coffee.letsgo.hangout.store;
 
+import coffee.letsgo.hangout.store.model.HangoutData;
+import coffee.letsgo.hangout.store.model.HangoutFolkData;
+import coffee.letsgo.storeland.CassandraSessionBuilder;
+import com.datastax.driver.core.Session;
+
+import java.util.List;
+
 /**
  * Created by xbwu on 10/19/14.
  */
 public class HangoutStoreCassandraImpl implements HangoutStore {
+
+    @Override
+    public void setHangout(HangoutData hangoutData) {
+        
+    }
+
+    @Override
+    public void setHangoutFolk(HangoutFolkData hangoutFolkData) {
+
+    }
+
+    @Override
+    public void setHangoutFolks(List<HangoutFolkData> hangoutFolks) {
+
+    }
+
+    @Override
+    public HangoutData getHangout(long hangoutId) {
+        return null;
+    }
+
+    @Override
+    public List<HangoutFolkData> getHangoutFolks(long hangoutId) {
+        return null;
+    }
+
+    @Override
+    public HangoutFolkData getHangoutFolk(long hangoutId, long userId) {
+        return null;
+    }
+
+    private static class CassandraSessionHolder {
+        private static Session session = new CassandraSessionBuilder("hangout").build();
+
+        private static Session getSession() {
+            return session;
+        }
+    }
 }
